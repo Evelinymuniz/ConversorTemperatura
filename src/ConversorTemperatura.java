cdaf06bb036eefe6c531c92827c235d9b948f3c6
Eveliny Muniz <107007338+Evelinymuniz@users.noreply.github.com>
2022-10-15 16:01:45 -0300

    Initial commit teste

---
+++ src/ConversorTemperatura.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ConversorTemperatura {
+
+    static double CelciusToFahrenheit(double celcius){
+        double fahrenheit = (celcius * 9/5) + 32;
+        return fahrenheit;
+    }
+    static double CelciusToFKelvin(double celcius){
+        double Kelvin = celcius + 273.15;
+        return Kelvin;
+    }
+    static void MostrarTemperatura(double valor, String temperatura){
+        System.out.println("A " + temperatura + "e " + valor );
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println ("####### CONVERSOR DE TEMPERATURA #######");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um valor da primeira temperatura em celsius para fahrenheit: ");
+        MostrarTemperatura(CelciusToFahrenheit(sc.nextDouble()),"fahrenheit ");
+        System.out.println("Digite um valor da segunda temperatura em celsius para fahrenheit: ");
+        MostrarTemperatura(CelciusToFahrenheit(sc.nextDouble()),"fahrenheit ");
+
+
+    }
+
+    }
+
+
