826be7cff885218830b1bbba98be641d0a0aef41
Eveliny Muniz <107007338+Evelinymuniz@users.noreply.github.com>
2022-10-15 16:32:19 -0300

    finalização.

--- src/ConversorTemperatura.java
+++ src/ConversorTemperatura.java
@@ -3,25 +3,32 @@ import java.util.Scanner;
 public class ConversorTemperatura {
 
     static double CelciusToFahrenheit(double celcius){
-        double fahrenheit = (celcius * 9/5) + 32;
+        final double FATOR1 = 9/5;
+        final double FATOR2 = 32;
+        double fahrenheit = (celcius * FATOR1) + FATOR2;
         return fahrenheit;
     }
-
     static void MostrarTemperatura(double valor, String temperatura){
-        System.out.println("A " + temperatura + "e " + valor );
+        System.out.println("A temperatura em " + temperatura + " é " + valor + "º");
     }
 
     public static void main(String[] args) {
 
         System.out.println ("####### CONVERSOR DE TEMPERATURA #######");
+
         Scanner sc = new Scanner(System.in);
-        System.out.println("Digite um valor da primeira temperatura em celsius para fahrenheit: ");
+
+        System.out.println("Digite o valor da primeira temperatura em celsius : ");
         MostrarTemperatura(CelciusToFahrenheit(sc.nextDouble()),"fahrenheit ");
-        System.out.println("Digite um valor da segunda temperatura em celsius para fahrenheit: ");
+
+        System.out.println("Digite o valor da segunda temperatura em celsius : ");
         MostrarTemperatura(CelciusToFahrenheit(sc.nextDouble()),"fahrenheit ");
+
+        System.out.println("obigada!!!");
 
 
     }
+
 
     }
 
